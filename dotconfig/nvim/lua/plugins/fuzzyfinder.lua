107f200510ad9b16d18ea58d600d683da9c6e048
BahadirAydin <bahadiraydin1923@gmail.com>
2025-04-18 23:37:42 +0300

    unused plugin

--- dotconfig/nvim/lua/plugins/fuzzyfinder.lua
+++ dotconfig/nvim/lua/plugins/fuzzyfinder.lua
@@ -44,11 +44,4 @@ return {
 			})
 		end,
 	},
-	{
-		"AckslD/nvim-neoclip.lua",
-		dependencies = { "nvim-telescope/telescope.nvim" },
-		config = function()
-			require("neoclip").setup()
-		end,
-	},
 }
