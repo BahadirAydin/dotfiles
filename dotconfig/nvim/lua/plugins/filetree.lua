9f9ea00932c2ca66c5295c9b0104e05e961300c6
BahadirAydin <bahadiraydin1923@gmail.com>
2025-05-21 20:58:40 +0300

    neotree apply migrations

--- dotconfig/nvim/lua/plugins/filetree.lua
+++ dotconfig/nvim/lua/plugins/filetree.lua
@@ -150,8 +150,11 @@ return {
 						--".null-ls_*",
 					},
 				},
-				follow_current_file = false, -- This will find and focus the file in the active buffer every
-				-- time the current file is changed while the tree is open.
+				follow_current_file = {
+					-- This will find and focus the file in the active buffer every
+					-- time the current file is changed while the tree is open.
+					enabled = true,
+				},
 				group_empty_dirs = false, -- when true, empty folders will be grouped together
 				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 				-- in whatever position is specified in window.position
@@ -185,8 +188,11 @@ return {
 				commands = {}, -- Add a custom command or override a global one using the same function name
 			},
 			buffers = {
-				follow_current_file = true, -- This will find and focus the file in the active buffer every
-				-- time the current file is changed while the tree is open.
+				follow_current_file = {
+					-- This will find and focus the file in the active buffer every
+					-- time the current file is changed while the tree is open.
+					enabled = true,
+				},
 				group_empty_dirs = true, -- when true, empty folders will be grouped together
 				show_unloaded = true,
 				window = {
