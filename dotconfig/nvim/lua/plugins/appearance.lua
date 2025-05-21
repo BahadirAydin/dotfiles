cb5467f6d028704b8a8b770cfa7b925ca851a379
BahadirAydin <bahadiraydin1923@gmail.com>
2025-05-21 20:57:48 +0300

    update indent-blankline to a much newer version and add noice

--- dotconfig/nvim/lua/plugins/appearance.lua
+++ dotconfig/nvim/lua/plugins/appearance.lua
@@ -11,19 +11,9 @@ return {
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		tag = "v2.20.8",
+		main = "ibl",
 		config = function()
-			require("indent_blankline").setup({
-				char = "",
-				context_char = "│",
-				filetype_exclude = { "text", "help", "markdown", "dashboard", "neo-tree" },
-				buftype_exclude = { "terminal", "nofile", "quickfix" },
-				show_current_context = true,
-				-- show_current_context_start = true,
-				strict_tabs = true,
-				show_trailing_blankline_indent = false,
-				max_indent_increase = 1,
-			})
+			require("ibl").setup({})
 		end,
 	},
 	{
@@ -37,9 +27,36 @@ return {
 				before = "",
 				after = "fg",
 			},
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
 		},
 	},
 }
