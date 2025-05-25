5786bf7c5d196a76a4e2a4b6b5f574406f8dff1c
BahadirAydin <bahadiraydin1923@gmail.com>
2025-05-26 00:24:16 +0300

    new colorscheme

--- dotconfig/nvim/lua/plugins/colorscheme.lua
+++ dotconfig/nvim/lua/plugins/colorscheme.lua
@@ -36,12 +36,21 @@ return {
 	-- 		vim.cmd([[colorscheme carbonfox]])
 	-- 	end,
 	-- },
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd([[colorscheme rose-pine]])
+	-- 	end,
+	-- },
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			vim.cmd([[colorscheme rose-pine]])
+			vim.cmd("colorscheme github_dark_default")
 		end,
 	},
 }
