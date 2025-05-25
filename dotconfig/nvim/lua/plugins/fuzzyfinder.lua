8975eb9c7fab911fe59e0d7a719fbdfe6536c0f2
BahadirAydin <bahadiraydin1923@gmail.com>
2025-05-26 00:25:30 +0300

    utilize fzf-lua for finding files and live grep, improve telescope

--- dotconfig/nvim/lua/plugins/fuzzyfinder.lua
+++ dotconfig/nvim/lua/plugins/fuzzyfinder.lua
@@ -4,8 +4,6 @@ return {
 		lazy = false,
 		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope Live Grep" },
 			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
 			{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Telescope Old Files" },
 			{ "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Telescope LSP Diagnostics" },
@@ -13,13 +11,20 @@ return {
 			{ "<leader>fa", "<cmd>Telescope aerial<cr>", desc = "Telescope Contexts" },
 			{ "<leader>fs", "<cmd>Telescope spell_suggest<cr>", desc = "Telescope Spell Suggest" },
 			{ "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Telescope Spell Suggest" },
+			{
+				"<leader>fi",
+				function()
+					require("telescope.builtin").symbols({ source = { "gitmoji" } })
+				end,
+				desc = "Find Gitmoji",
+			},
 		},
 		config = function()
 			local actions = require("telescope.actions")
 			require("telescope").load_extension("aerial")
 			require("telescope").setup({
 				defaults = {
-					file_ignore_patterns = { "node_modules/", "build/", "target/", "vendor/" },
+					file_ignore_patterns = { "node_modules/", "build/", "target/", "vendor/", "out/" },
 					mappings = {
 						i = {
 							["<esc>"] = actions.close,
@@ -28,20 +33,87 @@ return {
 					preview = {
 						filesize_limit = 0.1, -- MB
 					},
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+					},
+					layout_config = {
+						vertical = {
+							width = 0.8,
+						},
+					},
 				},
 				pickers = {
+					live_grep = {
+						layout_strategy = "vertical",
+					},
+					help_tags = {
+						layout_strategy = "vertical",
+					},
+					diagnostics = {
+						layout_strategy = "vertical",
+					},
+					lsp_references = {
+						layout_strategy = "vertical",
+					},
+					lsp_incoming_calls = {
+						layout_strategy = "vertical",
+					},
 					find_files = {
+						layout_strategy = "vertical",
 						find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
 					},
 					buffers = {
+						layout_strategy = "vertical",
 						sort_lastused = true,
 						ignore_current_buffer = true,
 					},
 					oldfiles = {
+						layout_strategy = "vertical",
 						sort_lastused = true,
+					},
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 					},
 				},
 			})
 		end,
+	},
+	{
+		"ibhagwan/fzf-lua",
+		dependencies = { "nvim-tree/nvim-web-devicons", optional = true },
+		cmd = { "FzfLua" },
+		keys = {
+			{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find All Files" },
+			{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
+		},
+		opts = {
+			hls = {
+				border = "FloatBorder",
+				preview_border = "FloatBorder",
+				help_border = "FloatBorder",
+				title = "TelescopeTitle",
+				preview_title = "TelescopePreviewTitle",
+			},
+			files = {
+				cmd = "rg --line-number --smart-case --no-ignore --no-ignore-exclude --files --hidden --follow",
+				rg_opts = "--line-number --smart-case --no-ignore --no-ignore-exclude --files --hidden --follow",
+			},
+			winopts = {
+				preview = {
+					layout = "vertical",
+				},
+			},
+		},
 	},
 }
