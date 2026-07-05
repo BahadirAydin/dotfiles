4cb5fab799c5273250d26b0a3d914d25d709780c
BahadirAydin <bahadiraydin1923@gmail.com>
2026-07-05 23:44:53 +0300

    🐛 fix(nvim): correct markdown config

--- private_dot_config/nvim/lua/plugins/markdown.lua
+++ private_dot_config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,9 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
-		config = function()
-			-- run = vim.fn["mkdp#util#install"]()
-			ft = "markdown"
+		ft = "markdown",
+		build = function()
+			vim.fn["mkdp#util#install"]()
 		end,
 	},
 	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
