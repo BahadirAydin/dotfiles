ed6154c43c990c4b02bfebdfd5cc9420d0985905
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 14:56:35 +0300

    refactor(nvim): treesitter configuration and dependencies

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,85 +1,72 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"regex",
-					"c",
-					"cpp",
-					"go",
-					"rust",
-					"vim",
-					"lua",
-					"python",
-					"bash",
-					"cmake",
-					"markdown",
-					"diff",
-					"json",
-					"toml",
-					"yaml",
-				},
-				sync_install = false,
-				auto_install = true,
-				ignore_install = { "dart" }, -- List of parsers to ignore installing
-				highlight = {
-					enable = true, -- false will disable the whole extension
-					disable = {}, -- list of language that will be disabled
-					additional_vim_regex_highlighting = false,
-				},
-				textobjects = {
-					select = {
-						enable = true,
-
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
-
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							-- You can optionally set descriptions to the mappings (used in the desc parameter of
-							-- nvim_buf_set_keymap) which plugins like which-key display
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-							-- You can also use captures from other query groups like `locals.scm`
-							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-						},
-						-- You can choose the select mode (default is charwise 'v')
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * method: eg 'v' or 'o'
-						-- and should return the mode ('v', 'V', or '<c-v>') or a table
-						-- mapping query_strings to modes.
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-						-- If you set this to `true` (default is `false`) then any textobject is
-						-- extended to include preceding or succeeding whitespace. Succeeding
-						-- whitespace has priority in order to act similarly to eg the built-in
-						-- `ap`.
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * selection_mode: eg 'v'
-						-- and should return true of false
-						include_surrounding_whitespace = true,
-					},
-				},
+			require("nvim-treesitter").install({
+				-- core nvim
+				"regex",
+				"vim",
+				"vimdoc",
+				"lua",
+				"query",
+				-- systems
+				"c",
+				"cpp",
+				"go",
+				"rust",
+				"python",
+				"bash",
+				"cmake",
+				"make",
+				"ninja",
+				-- web
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"scss",
+				"svelte",
+				-- docs/data
+				"markdown",
+				"markdown_inline",
+				"latex",
+				"json",
+				"toml",
+				"yaml",
+				"xml",
+				"sql",
+				"jq",
+				"diff",
+				-- git
+				"gitcommit",
+				"gitignore",
+				"git_rebase",
+				"gitattributes",
+				"git_config",
+				-- ops
+				"dockerfile",
+			})
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+			vim.o.foldcolumn = "1"
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("UserTreesitter", {}),
+				callback = function()
+					if pcall(vim.treesitter.start) then
+						vim.wo[0][0].foldmethod = "expr"
+						vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					end
+				end,
 			})
 		end,
 	},
 	{
 		"stevearc/aerial.nvim",
 		opts = {},
-		-- Optional dependencies
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons",
