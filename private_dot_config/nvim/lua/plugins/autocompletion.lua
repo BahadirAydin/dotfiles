68a69fc5ed58b944726ddf7334bf0fe2d6127dc7
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 10:35:23 +0300

    ✨ feat(nvim): add more capabilities of autocompletion
    Added appearance settings and enhanced completion options.

--- private_dot_config/nvim/lua/plugins/autocompletion.lua
+++ private_dot_config/nvim/lua/plugins/autocompletion.lua
@@ -4,8 +4,22 @@ return {
 	dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
 	opts = {
 		snippets = { preset = "luasnip" },
+		appearance = { nerd_font_variant = "mono" },
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				treesitter_highlighting = true,
+				window = { border = "rounded" },
+			},
+			menu = { border = "rounded" },
+			signature = {
+				enabled = true,
+				window = { border = "rounded" },
+			},
 		},
 	},
 }
