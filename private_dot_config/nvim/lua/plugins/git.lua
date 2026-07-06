fbf464e0c0f592557e832e9fb6365caf1a58e7a5
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 15:29:21 +0300

    feat(nvim): enhance git plugin configurations and key mappings
    Updated gitsigns.nvim configuration to include event triggers and enhanced key mappings with descriptions. Modified diffview.nvim key mappings for clarity.

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -1,9 +1,10 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-		opts = {},
+		event = { "BufReadPost", "BufNewFile" },
 		config = function()
 			require("gitsigns").setup({
+				current_line_blame = true,
 				on_attach = function(bufnr)
 					local gitsigns = require("gitsigns")
 
@@ -20,7 +21,7 @@ return {
 						else
 							gitsigns.nav_hunk("next")
 						end
-					end)
+					end, { desc = "Git: next hunk" })
 
 					map("n", "[c", function()
 						if vim.wo.diff then
@@ -28,58 +29,73 @@ return {
 						else
 							gitsigns.nav_hunk("prev")
 						end
-					end)
+					end, { desc = "Git: prev hunk" })
 
 					-- Actions
-					map("n", "<leader>hs", gitsigns.stage_hunk)
-					map("n", "<leader>hr", gitsigns.reset_hunk)
+					map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Git: stage hunk" })
+					map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Git: reset hunk" })
 
-					map("v", "<leader>hs", function()
+					map("x", "<leader>hs", function()
 						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end)
+					end, { desc = "Git: stage selection" })
 
-					map("v", "<leader>hr", function()
+					map("x", "<leader>hr", function()
 						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end)
+					end, { desc = "Git: reset selection" })
 
-					map("n", "<leader>hS", gitsigns.stage_buffer)
-					map("n", "<leader>hR", gitsigns.reset_buffer)
-					map("n", "<leader>hp", gitsigns.preview_hunk)
-					map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+					map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Git: stage buffer" })
+					map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Git: reset buffer" })
+					map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Git: preview hunk" })
+					map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Git: preview hunk inline" })
 
 					map("n", "<leader>hb", function()
 						gitsigns.blame_line({ full = true })
-					end)
+					end, { desc = "Git: blame line" })
 
-					map("n", "<leader>hd", gitsigns.diffthis)
+					map("n", "<leader>hd", gitsigns.diffthis, { desc = "Git: diff this" })
 
 					map("n", "<leader>hD", function()
 						gitsigns.diffthis("~")
-					end)
+					end, { desc = "Git: diff this ~" })
 
 					map("n", "<leader>hQ", function()
 						gitsigns.setqflist("all")
-					end)
-					map("n", "<leader>hq", gitsigns.setqflist)
+					end, { desc = "Git: quickfix all hunks" })
+					map("n", "<leader>hq", gitsigns.setqflist, { desc = "Git: quickfix hunks" })
 
 					-- Toggles
-					map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
-					map("n", "<leader>td", gitsigns.toggle_deleted)
-					map("n", "<leader>tw", gitsigns.toggle_word_diff)
+					map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Git: toggle line blame" })
+					map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Git: toggle deleted" })
+					map("n", "<leader>tw", gitsigns.toggle_word_diff, { desc = "Git: toggle word diff" })
 
 					-- Text object
-					map({ "o", "x" }, "ih", gitsigns.select_hunk)
+					map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Git: select hunk" })
 				end,
 			})
 		end,
 	},
 	{
 		"sindrets/diffview.nvim",
+		cmd = {
+			"DiffviewOpen",
+			"DiffviewClose",
+			"DiffviewToggleFiles",
+			"DiffviewFocusFiles",
+			"DiffviewFileHistory",
+			"DiffviewRefresh",
+		},
 		opts = {},
 		keys = {
 			{ "<leader>dd", "<cmd>DiffviewOpen<CR>", desc = "Diffview open", silent = true },
 			{ "<leader>dc", "<cmd>DiffviewClose<CR>", desc = "Diffview close", silent = true },
-			{ "<leader>df", "<cmd>DiffviewFileHistory<CR>", desc = "Diffview file history", silent = true },
+			{ "<leader>df", "<cmd>DiffviewFileHistory<CR>", desc = "Diffview repo history", silent = true },
+			{ "<leader>dF", "<cmd>DiffviewFileHistory %<CR>", desc = "Diffview file history", silent = true },
+			{ "<leader>dr", "<cmd>DiffviewRefresh<CR>", desc = "Diffview refresh", silent = true },
+			{ "<leader>do", "<cmd>DiffviewOpen origin/HEAD<CR>", desc = "Diffview vs origin/HEAD", silent = true },
+			{ "<leader>dO", "<cmd>DiffviewOpen origin/HEAD -- %<CR>", desc = "Diffview current file vs origin/HEAD", silent = true },
+			{ "<leader>dp", "<cmd>DiffviewOpen HEAD~1<CR>", desc = "Diffview vs HEAD~1", silent = true },
+			{ "<leader>dt", "<cmd>DiffviewToggleFiles<CR>", desc = "Diffview toggle files", silent = true },
+			{ "<leader>dh", "<esc><cmd>'<,'>DiffviewFileHistory<CR>", mode = "x", desc = "Diffview selection history", silent = true },
 		},
 	},
 }
