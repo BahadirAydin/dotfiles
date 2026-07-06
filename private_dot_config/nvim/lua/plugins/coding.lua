7a3bb3914a08a010c32d6728bf25bff927cd1a50
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 14:57:24 +0300

    chore: remove nvim-ufo plugin

--- private_dot_config/nvim/lua/plugins/coding.lua
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -26,22 +26,6 @@ return {
 		},
 	},
 	{
-		"kevinhwang91/nvim-ufo",
-		event = "VeryLazy",
-		dependencies = {
-			"kevinhwang91/promise-async",
-		},
-		config = function()
-			vim.o.foldcolumn = "1"
-			vim.o.foldlevel = 99
-			vim.o.foldlevelstart = 99
-			vim.o.foldenable = true
-			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-			require("ufo").setup()
-		end,
-	},
-	{
 		"MagicDuck/grug-far.nvim",
 		opts = {
 			startInInsertMode = false,
