463a80c524f4942dc6ee8ebc34b2059934de6c8a
BahadirAydin <bahadiraydin1923@gmail.com>
2026-07-06 21:19:51 +0300

    🚀 perf(nvim): remove current line blame true it is a perf cost

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -4,7 +4,6 @@ return {
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
 			require("gitsigns").setup({
-				current_line_blame = true,
 				on_attach = function(bufnr)
 					local gitsigns = require("gitsigns")
 
