5aa414e7a35859703448d25b65cd820159ea86db
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 09:58:30 +0300

    🐛 fix(nvim): replace deprecated usages

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -66,8 +66,8 @@ local M = {
 			"<cmd>lua vim.diagnostic.open_float(nil,{focus=false})<CR>",
 			{ silent = true, desc = "Floating diagnostics window." }
 		)
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, { desc = "Prev diagnostic" })
+        vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1,  float = true }) end, { desc = "Next diagnostic" })
 		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 		vim.api.nvim_create_autocmd("LspAttach", {
