cea3050368eba395d0addb683e5019dfddfa9823
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 11:21:23 +0300

    feat(nvim): update CMake options and key mappings

--- private_dot_config/nvim/lua/plugins/cmake.lua
+++ private_dot_config/nvim/lua/plugins/cmake.lua
@@ -3,6 +3,12 @@ return {
 	{
 		"Civitasv/cmake-tools.nvim",
 		opts = {
+			cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
+			cmake_regenerate_on_save = true,
+			cmake_use_preset = true,
+			cmake_compile_commands_options = {
+				action = "copy",
+			},
 			cmake_build_directory = function()
 				local osys = require("cmake-tools.osys")
 				if osys.iswin32 then
@@ -10,17 +16,13 @@ return {
 				end
 				return "out/${variant:buildType}"
 			end,
-			cmake_notifications = {
-				runner = { enabled = false },
-				executor = { enabled = false },
-			},
 		},
 		keys = {
-			{ "<leader>cg", "<cmd>CMakeGenerate Ninja<cr>", desc = "CMake: Generate" },
+			{ "<leader>cg", "<cmd>CMakeGenerate -G Ninja<cr>", desc = "CMake: Generate" },
 			{ "<leader>cb", "<cmd>CMakeBuild<cr>", desc = "CMake: Build" },
 			{ "<leader>cc", "<cmd>CMakeClean<cr>", desc = "CMake: Clean" },
 			{ "<leader>ct", "<cmd>CMakeSelectBuildType<cr>", desc = "CMake: Select build type" },
-			{ "<leader>cS", "<cmd>CMakeSelectLaunchTarget<cr>", desc = "CMake: Select target" },
+			{ "<leader>cs", "<cmd>CMakeSelectLaunchTarget<cr>", desc = "CMake: Select target" },
 			{ "<leader>cr", "<cmd>CMakeRun<cr>", desc = "CMake: Run" },
 			{ "<leader>ci", "<cmd>CMakeInstall<cr>", desc = "CMake: Install" },
 		},
