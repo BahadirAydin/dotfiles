de30a2cf3fa5260c69124fd959f1a333057588f1
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-06 14:09:03 +0300

    feat(nvim): update Noice.nvim configuration for LSP features

--- private_dot_config/nvim/lua/plugins/appearance.lua
+++ private_dot_config/nvim/lua/plugins/appearance.lua
@@ -26,20 +26,35 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-				},
-			},
 			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+			lsp = {
+				override = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+				progress = {
+					enabled = true,
+					view = "mini",
+					throttle = 1000 / 10,
+				},
+				hover = { silent = true },
+				signature = {
+					enabled = true,
+					auto_open = {
+						enabled = true,
+						trigger = true,
+						luasnip = true,
+						throttle = 50,
+					},
+				},
 			},
 		},
 		dependencies = {
