d82f6d57178d0c52b79fdb8d3dd21597778623bd
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-07-07 11:01:51 +0300

    feat(nvim): fully migrate to fzf-lua
    Removed Telescope configuration and replaced with FzfLua key mappings and options.

--- private_dot_config/nvim/lua/plugins/fuzzyfinder.lua
+++ private_dot_config/nvim/lua/plugins/fuzzyfinder.lua
@@ -1,88 +1,18 @@
 return {
 	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		keys = {
-			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
-			{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Telescope Old Files" },
-			{ "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Telescope LSP Diagnostics" },
-			{ "<leader>fh", "<cmd>Telescope command_history<cr>", desc = "Telescope Command History" },
-			{ "<leader>fs", "<cmd>Telescope spell_suggest<cr>", desc = "Telescope Spell Suggest" },
-			{
-				"<leader>fi",
-				function()
-					require("telescope.builtin").symbols({ source = { "gitmoji" } })
-				end,
-				desc = "Find Gitmoji",
-			},
-		},
-		config = function()
-			local actions = require("telescope.actions")
-			require("telescope").setup({
-				defaults = {
-					file_ignore_patterns = { "node_modules/", "build/", "target/", "vendor/", "out/" },
-					mappings = {
-						i = {
-							["<esc>"] = actions.close,
-						},
-					},
-					preview = {
-						filesize_limit = 0.1, -- MB
-					},
-					vimgrep_arguments = vim.fn.executable("rg") == 1 and {
-						"rg",
-						"--color=never",
-						"--no-heading",
-						"--with-filename",
-						"--line-number",
-						"--column",
-						"--smart-case",
-					} or nil,
-					layout_config = {
-						vertical = {
-							width = 0.8,
-						},
-					},
-				},
-				pickers = {
-					help_tags = {
-						layout_strategy = "vertical",
-					},
-					diagnostics = {
-						layout_strategy = "vertical",
-					},
-					lsp_references = {
-						layout_strategy = "vertical",
-					},
-					lsp_incoming_calls = {
-						layout_strategy = "vertical",
-					},
-					find_files = {
-						layout_strategy = "vertical",
-						find_command = vim.fn.executable("rg") == 1 and { "rg", "--files", "--hidden", "-g", "!.git" }
-							or nil,
-					},
-					buffers = {
-						layout_strategy = "vertical",
-						sort_lastused = true,
-						ignore_current_buffer = true,
-					},
-					oldfiles = {
-						layout_strategy = "vertical",
-						sort_lastused = true,
-					},
-				},
-			})
-		end,
-	},
-	{
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons", optional = true },
 		cmd = { "FzfLua" },
 		keys = {
-			{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find All Files" },
-			{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Live Grep" },
-			{ "<leader>fr", "<cmd>FzfLua resume<cr>", desc = "Resume Search" },
+			{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "FzfLua: Files" },
+			{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "FzfLua: Live Grep" },
+			{ "<leader>fr", "<cmd>FzfLua resume<cr>", desc = "FzfLua: Resume Search" },
+			{ "<leader>b", "<cmd>FzfLua buffers<cr>", desc = "FzfLua: Buffers" },
+			{ "<leader>fo", "<cmd>FzfLua oldfiles<cr>", desc = "FzfLua: Old Files" },
+			{ "<leader>fh", "<cmd>FzfLua command_history<cr>", desc = "FzfLua: Command History" },
+			{ "<leader>fs", "<cmd>FzfLua git_status<cr>", desc = "FzfLua: Git Status" },
+			{ "<leader>fd", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "FzfLua: Diagnostics Workspace" },
+			{ "<leader>fl", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "FzfLua: LSP Document Symbols" },
 			{
 				"<leader>fc",
 				function()
@@ -101,16 +31,12 @@ return {
 			},
 		},
 		opts = {
-			hls = {
-				border = "FloatBorder",
-				preview_border = "FloatBorder",
-				help_border = "FloatBorder",
-				title = "TelescopeTitle",
-				preview_title = "TelescopePreviewTitle",
+			oldfiles = {
+				include_current_session = true,
 			},
-			winopts = {
-				preview = {
-					layout = "vertical",
+			previewers = {
+				builtin = {
+					syntax_limit_b = 1024 * 100, -- 100KB
 				},
 			},
 		},
