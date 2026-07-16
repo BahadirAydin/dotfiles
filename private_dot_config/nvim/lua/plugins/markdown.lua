9057bcad718a209ff36f744e22b9dcaadd3f0309
BahadirAydin <bahadiraydin1923@gmail.com>
2026-07-16 23:42:49 +0300

    feat(nvim): migrate to render-markdown.nvim

--- private_dot_config/nvim/lua/plugins/markdown.lua
+++ private_dot_config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,9 @@
 return {
 	{
-		"iamcco/markdown-preview.nvim",
-		ft = "markdown",
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		ft = { "markdown", "norg", "rmd", "org" },
+		opts = {},
 	},
 }
+
