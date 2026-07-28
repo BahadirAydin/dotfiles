16f12dfa81a55aa5bf59914c1cae9da7604ef62f
BahadirAydin <bahadiraydin1923@gmail.com>
2026-07-28 23:40:56 +0300

    feat(nvim): mason -- auto install most used

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,29 @@
 return {
-	"mason-org/mason.nvim",
-	opts = {},
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			auto_update = false,
+			ensure_installed = {
+				-- language servers
+				"clangd",
+				"cmake-language-server",
+				"basedpyright",
+				"ruff",
+				"json-lsp",
+				"yaml-language-server",
+				"marksman",
+				"rust-analyzer",
+				-- formatters
+				"stylua",
+				"clang-format",
+				"gersemi",
+				"prettierd",
+			},
+		},
+	},
 }
