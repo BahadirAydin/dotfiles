6bd20426160b8c78e37a40d7bd26362cfdba9357
Bahadır Aydın <bahadiraydin1923@gmail.com>
2026-08-03 13:31:49 +0300

    fix(nvim): remove deprecated task info key from overseer

--- private_dot_config/nvim/lua/plugins/overseer.lua
+++ private_dot_config/nvim/lua/plugins/overseer.lua
@@ -6,7 +6,6 @@ return {
 			{ "<leader>rr", "<cmd>OverseerRun<CR>", desc = "Run task" },
 			{ "<leader>rt", "<cmd>OverseerToggle<CR>", desc = "Toggle task list" },
 			{ "<leader>ra", "<cmd>OverseerTaskAction<CR>", desc = "Task action" },
-			{ "<leader>ri", "<cmd>OverseerInfo<CR>", desc = "Task info" },
 		},
 	},
 }
