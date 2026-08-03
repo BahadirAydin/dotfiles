57a9e585453daf6ab8b9007a0d5830bef9140ed4
BahadirAydin <bahadiraydin1923@gmail.com>
2026-08-03 20:44:03 +0300

    refactor(nvim): better integration

--- private_dot_config/nvim/lua/plugins/overseer.lua
+++ private_dot_config/nvim/lua/plugins/overseer.lua
@@ -1,9 +1,19 @@
 return {
 	{
 		"stevearc/overseer.nvim",
-		opts = {},
+		opts = {
+			component_aliases = {
+				default = {
+					"on_exit_set_status",
+					"on_complete_notify",
+					{ "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+					{ "open_output", on_start = "never", on_complete = "failure", focus = true },
+				},
+			},
+		},
 		keys = {
 			{ "<leader>rr", "<cmd>OverseerRun<CR>", desc = "Run task" },
+			{ "<leader>rc", "<cmd>OverseerShell<CR>", desc = "Run shell command" },
 			{ "<leader>rt", "<cmd>OverseerToggle<CR>", desc = "Toggle task list" },
 			{ "<leader>ra", "<cmd>OverseerTaskAction<CR>", desc = "Task action" },
 		},
