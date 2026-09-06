2a6d6d3e7e1456a2191434e3b4fa1782c0be64ad
BahadirAydin <bahadiraydin1923@gmail.com>
2026-09-06 22:42:36 +0300

    refactor(dunst): wpctl for volume

--- private_dot_config/dunst/executable_volume.sh
+++ private_dot_config/dunst/executable_volume.sh
@@ -1,62 +1,52 @@
 #!/bin/sh
 set -eu
-# You can call this script like this:
-# $./volume.sh up
-# $./volume.sh down
-# $./volume.sh mute
+# $./volume.sh up | down | mute
 
-# icons
+SINK="@DEFAULT_AUDIO_SINK@"
+STEP="5%"
+LIMIT="2.0"   # 200%, matching the old --allow-boost --set-limit 200
+
 audio_icon_high="audio-volume-high-symbolic"
 audio_icon_medium="audio-volume-medium-symbolic"
 audio_icon_low="audio-volume-low-symbolic"
 mute_icon="audio-volume-muted-blocking-symbolic"
 
-# output
-# use the following option for pulseaudio
-#output_name="pulse"
-# use the following option for alsa
-output_name="pulse"
-
-
-
-get_volume () {
-    pamixer --get-volume
+# "Volume: 0.55" or "Volume: 0.55 [MUTED]"
+get_volume() {
+    wpctl get-volume "$SINK" | awk '{ printf "%d", $2 * 100 }'
 }
 
 is_mute() {
-    pamixer --get-mute
+    wpctl get-volume "$SINK" | grep -q MUTED
 }
 
-send_notification () {
-    # Send the notification
-    dunstify -i "$audio_icon" -t 1600 -h string:x-dunst-stack-tag:volume -u normal "Volume" -h int:value:"$(get_volume)"
+send_notification() {
+    vol=$(get_volume)
+    if [ "$vol" -ge 70 ]; then
+        audio_icon="$audio_icon_high"
+    elif [ "$vol" -ge 50 ]; then
+        audio_icon="$audio_icon_medium"
+    else
+        audio_icon="$audio_icon_low"
+    fi
+    dunstify -i "$audio_icon" -t 1600 -h string:x-dunst-stack-tag:volume -u normal "Volume" -h int:value:"$vol"
 }
-
-if [ "$(get_volume)" -ge 70 ]; then
-    audio_icon="$audio_icon_high"
-elif [ "$(get_volume)" -ge 50 ]; then
-    audio_icon="$audio_icon_medium"
-else
-    audio_icon="$audio_icon_low"
-fi
 
 case $1 in
     up)
-    pamixer --allow-boost --set-limit 200 -ui 5
-	send_notification
-	;;
+        wpctl set-volume -l "$LIMIT" "$SINK" "$STEP+"
+        send_notification
+        ;;
     down)
-    pamixer --allow-boost --set-limit 200 -ud 5
-	send_notification
-	;;
+        wpctl set-volume "$SINK" "$STEP-"
+        send_notification
+        ;;
     mute)
-    pamixer --toggle-mute
-	if is_mute ; then
-	    dunstify -i "$mute_icon" -t 1600 -h string:x-dunst-stack-tag:volume -u normal "Mute"
-	else
-	    send_notification
-	fi
-	;;
+        wpctl set-mute "$SINK" toggle
+        if is_mute; then
+            dunstify -i "$mute_icon" -t 1600 -h string:x-dunst-stack-tag:volume -u normal "Mute"
+        else
+            send_notification
+        fi
+        ;;
 esac
-
-
