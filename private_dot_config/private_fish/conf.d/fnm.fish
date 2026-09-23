02a78b0acc9e3edf3696ef46cc541aa1c6a82b2d
BahadirAydin <bahadiraydin1923@gmail.com>
2026-09-23 23:04:41 +0300

    perf(fish): lazy-load fnm

--- private_dot_config/private_fish/conf.d/fnm.fish
+++ private_dot_config/private_fish/conf.d/fnm.fish
@@ -1,6 +1,11 @@
-# fnm
-set FNM_PATH "/home/bahadir/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
+set FNM_PATH "$HOME/.local/share/fnm"
+if test -d $FNM_PATH
+    set PATH $FNM_PATH $PATH
+    for cmd in node npm npx corepack
+        function $cmd --inherit-variable cmd
+            functions --erase node npm npx corepack
+            fnm env | source
+            $cmd $argv
+        end
+    end
 end
