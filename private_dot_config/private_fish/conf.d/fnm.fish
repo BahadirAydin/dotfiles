c738296d4aad92a98b828f85162d3441a12062ad
BahadirAydin <bahadiraydin1923@gmail.com>
2026-09-24 22:10:54 +0300

    feat(fish): pnpm also loads

--- private_dot_config/private_fish/conf.d/fnm.fish
+++ private_dot_config/private_fish/conf.d/fnm.fish
@@ -1,11 +1,11 @@
 set FNM_PATH "$HOME/.local/share/fnm"
 if test -d $FNM_PATH
     set PATH $FNM_PATH $PATH
-    for cmd in node npm npx corepack
+    for cmd in node npm npx corepack pnpm
         function $cmd --inherit-variable cmd
-            functions --erase node npm npx corepack
+            functions --erase node npm npx corepack pnpm
             fnm env | source
-            $cmd $argv
+            command $cmd $argv
         end
     end
 end
